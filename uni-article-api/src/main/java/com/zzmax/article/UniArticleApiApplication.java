cb13294facf9ae79dbbbd681c88042594b71880c
zzmax0625 <lmxxmlsq@163.com>
2022-03-22 18:59:52 +0800

    完成账号密码登陆接口

--- uni-article-api/src/main/java/com/zzmax/article/UniArticleApiApplication.java
+++ uni-article-api/src/main/java/com/zzmax/article/UniArticleApiApplication.java
@@ -1,9 +1,12 @@
 package com.zzmax.article;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.zzmax.article.mapper")
 public class UniArticleApiApplication {
 
     public static void main(String[] args) {
